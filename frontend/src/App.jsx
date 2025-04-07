6284c021c9a5022c149b59cf1dc8bf677dc86611
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2025-04-07 11:51:21 +0300

    Committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Intro from './components/Intro';
 import About from './components/About';
 import Projects from './components/Projects';
-import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
@@ -17,7 +16,6 @@ function App() {
           <Intro />
           <About />
           <Projects />
-          <Experience />
           <Contact />
         </main>
         <Footer />
